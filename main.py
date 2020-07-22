4a608d3c684975e01d0357330e5971088d87e425
TheSharkhead2 <68621893+TheSharkhead2@users.noreply.github.com>
2020-07-21 17:51:24 -0700

    Created "main.py" and "dependencies.txt"

---
+++ main.py
@@ -0,0 +1,13 @@
+#main file
+#information:
+#citations in comments (links provided)
+#libraries used in "dependencies.txt"
+#relying on pygame for visual library
+
+import pygame
+from pygame.locals import *
+from screeninfo import get_monitors #found in: https://stackoverflow.com/questions/3129322/how-do-i-get-monitor-resolution-in-python
+
+class Game:
+
+    window 
