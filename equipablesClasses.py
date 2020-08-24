746b93a173b578f1d852f8d39267e24c3f91b981
TheSharkhead2 <68621893+TheSharkhead2@users.noreply.github.com>
2020-08-24 15:23:21 -0700

    started work on weapon damage system

--- equipablesClasses.py
+++ equipablesClasses.py
@@ -1,3 +1,4 @@
+import random
 
 class Weapon:
 
@@ -10,6 +11,8 @@ class Weapon:
     bypassBlockChanceBase = 1 #how much more/less likely you are to break through opponent block. See above for affects
     counterAttackChanceBase = 0.5 #how likely a counter attack is to succeed with this weapon
     avoidCounterAttackChanceBase = 1 #how resistant you are to counter attacks with this weapon. Same affects as "bypassBlockChanceBase" on "blockChanceBase" (during swap, stat comes from weapon being swapped out)
+
+    rarityAttackMulti = {"common" : 1, "uncommon" : 1.1, "rare" : 1.3, "epic" : 1.5, "legendary" : 1.75} #multiplier for damage with weapons at different rarities. May be revisited in future for balancing
 
     def __init__(self, name, rarity, level, attributes, baseDamage, critMulti, critChance, damageConsistency, swapSpeed, blockChance, bypassBlockChance, counterAttackChance, avoidCounterAttackChance): #variables: [critMulti, critChance, damageConsistency, swapSpeed, blockChance, bypassBlockChance, counterAttackChance] represent scalers for differences between weapon classes
         self.name = name #weapon name
@@ -25,3 +28,6 @@ class Weapon:
         self.bypassBlockChance = self.bypassBlockChanceBase * bypassBlockChance
         self.counterAttackChance = self.counterAttackChanceBase * counterAttackChance
         self.avoidCounterAttackChance = self.avoidCounterAttackChanceBase * avoidCounterAttackChance
+    
+    def calculate_damage(self, crit=False):
+        damage = self.baseDamage * self.rarityAttackMulti[self.rarity] 
