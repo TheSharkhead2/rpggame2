f36d73331fba2216a92c906f04cb08456ec1307d
TheSharkhead2 <68621893+TheSharkhead2@users.noreply.github.com>
2020-08-24 18:14:28 -0700

    Added to damage calculation system for weapons

--- equipablesClasses.py
+++ equipablesClasses.py
@@ -13,13 +13,14 @@ class Weapon:
     avoidCounterAttackChanceBase = 1 #how resistant you are to counter attacks with this weapon. Same affects as "bypassBlockChanceBase" on "blockChanceBase" (during swap, stat comes from weapon being swapped out)
 
     rarityAttackMulti = {"common" : 1, "uncommon" : 1.1, "rare" : 1.3, "epic" : 1.5, "legendary" : 1.75} #multiplier for damage with weapons at different rarities. May be revisited in future for balancing
+    levelMulti = 7 #multiplier for damage of weapon based on level (ie base damage * levelMulti * level). Up to change
 
     def __init__(self, name, rarity, level, attributes, baseDamage, critMulti, critChance, damageConsistency, swapSpeed, blockChance, bypassBlockChance, counterAttackChance, avoidCounterAttackChance): #variables: [critMulti, critChance, damageConsistency, swapSpeed, blockChance, bypassBlockChance, counterAttackChance] represent scalers for differences between weapon classes
         self.name = name #weapon name
         self.rarity = rarity #how rare weapon is. Also affects attributes (number and quality) and damage
         self.level = level #what level the weapon is. Affects damage. Player can only use weapons at or below their level
         self.attributes = attributes #special affects the weapon has on the player. 
-        self.baseDamage = baseDamage #damage with "level 0," no rarity, no other modifiers. Scaled up for high levels and rarity, as well as weapon class.
+        self.baseDamage = baseDamage #damage with "level 0," no rarity, no other modifiers. Scaled up for high levels and rarity, as well as weapon class. 
         self.critMulti = self.critMultiplierBase * critMulti
         self.critChance = self.critChanceBase * critChance
         self.damageConsistency = self.damageConsistencyBase * damageConsistency
@@ -29,5 +30,13 @@ class Weapon:
         self.counterAttackChance = self.counterAttackChanceBase * counterAttackChance
         self.avoidCounterAttackChance = self.avoidCounterAttackChanceBase * avoidCounterAttackChance
     
+    def _base_damage(self):
+        return(self.baseDamage * self.rarityAttackMulti[self.rarity]) #return scaled damage of weapon
+
     def calculate_damage(self, crit=False):
-        damage = self.baseDamage * self.rarityAttackMulti[self.rarity] 
+        damageDeltaMulti = random.random() * self.damageConsistency + 1 #calculate random multiplier for damage within damage inconsistancy range 
+        if random.randint(1,2) == 2: 
+            damageDeltaMulti *= -1 #flip half of these multipliers to negative (loss in damage)
+
+        damage = self._base_damage() * damageDeltaMulti
+        
